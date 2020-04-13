abf39378fab19d960244bb9203a03ba0d1706013
suckgamony <suckgamony@gmail.com>
2020-04-13 18:59:05 +0900

    Add more regional decoding test

--- library/src/androidTest/java/com/github/suckgamony/lazybitmapdecoder/decoder/RegionalSourceBitmapDecoderTest.kt
+++ library/src/androidTest/java/com/github/suckgamony/lazybitmapdecoder/decoder/RegionalSourceBitmapDecoderTest.kt
@@ -7,6 +7,9 @@ import android.graphics.Rect
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.github.suckgamony.lazybitmapdecoder.InstrumentedTestBase
+import com.github.suckgamony.lazybitmapdecoder.source.ByteArrayBitmapSource
+import com.github.suckgamony.lazybitmapdecoder.source.FileBitmapSource
+import com.github.suckgamony.lazybitmapdecoder.source.InputStreamBitmapSource
 import com.github.suckgamony.lazybitmapdecoder.source.ResourceBitmapSource
 import com.github.suckgamony.lazybitmapdecoder.test.R
 import org.junit.Assert
@@ -15,6 +18,8 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 @RunWith(AndroidJUnit4::class)
 class RegionalSourceBitmapDecoderTest : InstrumentedTestBase() {
@@ -76,7 +81,126 @@ class RegionalSourceBitmapDecoderTest : InstrumentedTestBase() {
             val decoder = RegionalSourceBitmapDecoder(source, Rect(12, 34, 12 + 56, 34 + 78))
             val byDecoder = assertNotNull(decoder.decode())
 
-            assertEquals(byDecoder, byFactory, 0x0c)
+            assertEquals(byDecoder, byFactory, TOLERANCE)
         }
+    }
+
+    @Test
+    fun decodeByteArray() {
+        val byFactoryFromRes = BitmapFactory.decodeResource(appContext.resources, R.drawable.nodpi_image)
+        val data = ByteArrayOutputStream().use {
+            byFactoryFromRes.compress(Bitmap.CompressFormat.PNG, 100, it)
+            it.toByteArray()
+        }
+
+        val byFactoryFromByteArray = Bitmap.createBitmap(
+            BitmapFactory.decodeByteArray(data, 0, data.size),
+            12, 34, 56, 78
+        )
+
+        val source = ByteArrayBitmapSource(data, 0, data.size)
+        val decoder = RegionalSourceBitmapDecoder(source, Rect(12, 34, 12 + 56, 34 + 78))
+        val byDecoder = assertNotNull(decoder.decode())
+
+        assertEquals(byDecoder, byFactoryFromByteArray, TOLERANCE)
+    }
+
+    @Test
+    fun decodeByteArrayWithMetadata() {
+        val byFactoryFromRes = BitmapFactory.decodeResource(appContext.resources, R.drawable.nodpi_image)
+        val data = ByteArrayOutputStream().use {
+            byFactoryFromRes.compress(Bitmap.CompressFormat.PNG, 100, it)
+            it.toByteArray()
+        }
+
+        val byFactoryFromByteArray = Bitmap.createBitmap(
+            BitmapFactory.decodeByteArray(data, 0, data.size),
+            12, 34, 56, 78
+        )
+
+        val source = ByteArrayBitmapSource(data, 0, data.size)
+        val decoder = RegionalSourceBitmapDecoder(source, Rect(12, 34, 12 + 56, 34 + 78))
+        assertEquals(decoder.width, byFactoryFromByteArray.width)
+        assertEquals(decoder.height, byFactoryFromByteArray.height)
+
+        val byDecoder = assertNotNull(decoder.decode())
+        assertEquals(byDecoder, byFactoryFromByteArray, TOLERANCE)
+    }
+
+    @Test
+    fun decodeFile() {
+        val byFactoryFromRes = BitmapFactory.decodeResource(appContext.resources, R.drawable.nodpi_image)
+        appContext.openFileOutput("decodeFileTest.png", Context.MODE_PRIVATE).use {
+            byFactoryFromRes.compress(Bitmap.CompressFormat.PNG, 100, it)
+        }
+
+        val file = File(appContext.filesDir, "decodeFileTest.png")
+        val byFactoryFromFile = Bitmap.createBitmap(
+            BitmapFactory.decodeFile(file.path),
+            12, 34, 56, 78
+        )
+
+        val source = FileBitmapSource(file)
+        val decoder = RegionalSourceBitmapDecoder(source, Rect(12, 34, 12 + 56, 34 + 78))
+        val byDecoder = assertNotNull(decoder.decode())
+
+        assertEquals(byDecoder, byFactoryFromFile, TOLERANCE)
+    }
+
+    @Test
+    fun decodeFileWithMetadata() {
+        val byFactoryFromRes = BitmapFactory.decodeResource(appContext.resources, R.drawable.nodpi_image)
+        appContext.openFileOutput("decodeFileTest.png", Context.MODE_PRIVATE).use {
+            byFactoryFromRes.compress(Bitmap.CompressFormat.PNG, 100, it)
+        }
+
+        val file = File(appContext.filesDir, "decodeFileTest.png")
+        val byFactoryFromFile = Bitmap.createBitmap(
+            BitmapFactory.decodeFile(file.path),
+            12, 34, 56, 78
+        )
+
+        val source = FileBitmapSource(file)
+        val decoder = RegionalSourceBitmapDecoder(source, Rect(12, 34, 12 + 56, 34 + 78))
+        assertEquals(decoder.width, byFactoryFromFile.width)
+        assertEquals(decoder.height, byFactoryFromFile.height)
+
+        val byDecoder = assertNotNull(decoder.decode())
+        assertEquals(byDecoder, byFactoryFromFile, TOLERANCE)
+    }
+
+    @Test
+    fun decodeStream() {
+        val byFactory = Bitmap.createBitmap(
+            BitmapFactory.decodeStream(appContext.resources.openRawResource(R.drawable.nodpi_image)),
+            12, 34, 56, 78
+        )
+
+        val source = InputStreamBitmapSource { appContext.resources.openRawResource(R.drawable.nodpi_image) }
+        val decoder = RegionalSourceBitmapDecoder(source, Rect(12, 34, 12 + 56, 34 + 78))
+        val byDecoder = assertNotNull(decoder.decode())
+
+        assertEquals(byDecoder, byFactory)
+    }
+
+    @Test
+    fun decodeStreamWithMetadata() {
+        val byFactory = Bitmap.createBitmap(
+            BitmapFactory.decodeStream(appContext.resources.openRawResource(R.drawable.nodpi_image)),
+            12, 34, 56, 78
+        )
+
+        val source = InputStreamBitmapSource { appContext.resources.openRawResource(R.drawable.nodpi_image) }
+        val decoder = RegionalSourceBitmapDecoder(source, Rect(12, 34, 12 + 56, 34 + 78))
+
+        assertEquals(decoder.width, byFactory.width)
+        assertEquals(decoder.height, byFactory.height)
+
+        val byDecoder = assertNotNull(decoder.decode())
+        assertEquals(byDecoder, byFactory)
+    }
+
+    companion object {
+        private const val TOLERANCE = 0x0c
     }
 }
