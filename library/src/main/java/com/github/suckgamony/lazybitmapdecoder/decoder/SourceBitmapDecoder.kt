d594d7d3dff8b88b77ad95e89392f4be5ae3d3db
suckgamony <suckgamony@gmail.com>
2020-04-19 17:45:20 +0900

    Merge BaseSourceBitmapDecoder into SourceBitmapDecoder

--- library/src/main/java/com/github/suckgamony/lazybitmapdecoder/decoder/SourceBitmapDecoder.kt
+++ library/src/main/java/com/github/suckgamony/lazybitmapdecoder/decoder/SourceBitmapDecoder.kt
@@ -1,34 +1,74 @@
 package com.github.suckgamony.lazybitmapdecoder.decoder
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.annotation.GuardedBy
+import com.github.suckgamony.lazybitmapdecoder.BitmapDecoder
 import com.github.suckgamony.lazybitmapdecoder.BitmapSource
 import com.github.suckgamony.lazybitmapdecoder.DecodingParametersBuilder
 
 internal class SourceBitmapDecoder(
     internal val source: BitmapSource
-) : BaseSourceBitmapDecoder() {
+) : BitmapDecoder() {
+    private val boundsDecodeLock = Any()
+
+    @GuardedBy("boundsDecodeLock")
+    private var widthDecoded = -1
+    @GuardedBy("boundsDecodeLock")
+    private var heightDecoded = -1
+    @GuardedBy("boundsDecodeLock")
+    private var densityScale = 1f
+
+    private val boundsDecoded
+        @GuardedBy("boundsDecodeLock")
+        get() = widthDecoded != -1
+
     override val width: Int
         get() {
             synchronized(boundsDecodeLock) {
-                decodeBounds(source)
+                decodeBounds()
                 return widthDecoded
             }
         }
     override val height: Int
         get() {
             synchronized(boundsDecodeLock) {
-                decodeBounds(source)
+                decodeBounds()
                 return heightDecoded
             }
         }
+
+    @GuardedBy("boundsDecodeLock")
+    private fun decodeBounds() {
+        if (boundsDecoded) {
+            return
+        }
+
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+
+        source.decodeBitmap(options)
+        copyMetadata(options)
+    }
+
+    @GuardedBy("boundsDecodeLock")
+    private fun copyMetadata(options: BitmapFactory.Options) {
+        widthDecoded = options.outWidth
+        heightDecoded = options.outHeight
+        if (options.inScaled && options.inDensity != 0 && options.inTargetDensity != 0) {
+            densityScale = options.inTargetDensity.toFloat() / options.inDensity
+        }
+    }
 
     override fun makeParameters(flags: Int): DecodingParametersBuilder {
         return if ((flags and DecodingParametersBuilder.FLAG_REGIONAL) != 0) {
             val parametersBuilder = super.makeParameters(flags)
             if (source.manualDensityScalingForRegional) {
-                decodeBounds(source)
-                parametersBuilder.scaleX *= densityScale
-                parametersBuilder.scaleY *= densityScale
+                synchronized(boundsDecodeLock) {
+                    decodeBounds()
+                    parametersBuilder.scaleX *= densityScale
+                    parametersBuilder.scaleY *= densityScale
+                }
             }
             parametersBuilder
         } else {
@@ -37,9 +77,15 @@ internal class SourceBitmapDecoder(
     }
 
     override fun decode(parametersBuilder: DecodingParametersBuilder): Bitmap? {
+        val params = parametersBuilder.buildParameters()
+        if (params.region != null) {
+            synchronized(boundsDecodeLock) {
+                decodeBounds()
+            }
+        }
+
         source.onDecodeStarted()
         try {
-            val params = parametersBuilder.buildParameters()
             val bitmap = if (params.region != null) {
                 source.decodeBitmapRegion(params.region, params.options)
             } else {
